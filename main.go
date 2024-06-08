a87bc206e8540b6204351bc4d37549e956048d3a
Trevor Farias <levelonethousandr@gmail.com>
2024-06-07 23:54:30 -0700

    modified api endpoints hosted on google app cloud

--- main.go
+++ main.go
@@ -1,18 +1,100 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello")
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var client *mongo.Client
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	collection := client.Database("yourDatabase").Collection("users")
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	var users []User
+
+	if err := cursor.All(context.TODO(), &users); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	collection := client.Database("yourDatabase").Collection("users")
+	_, err := collection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(newUser)
 }
 
 func main() {
-    http.HandleFunc("/api/hello", helloHandler)
-    fmt.Println("Starting server on port 8080...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGODB_URI")
+
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+
+	client, err = mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+
+	// Enable CORS
+	corsHandler := cors.Default().Handler(router)
+
+	fmt.Println("Listening at port :8080")
+	log.Fatal(http.ListenAndServe(":8080", corsHandler))
 }
